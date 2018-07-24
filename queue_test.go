a2a870fb4bf8f701be677499f7819af48dd2112e
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-07-24 22:00:55 +0200

    Make queue tests more restrictive

--- queue_test.go
+++ queue_test.go
@@ -2,49 +2,55 @@ package triangolatte
 
 import "testing"
 
+func checkQueue(t *testing.T, q *Queue, expected []*Element) {
+	for i := 0; i < q.capacity; i++ {
+		if q.elements[i] != expected[i] {
+			t.Error("Error")
+		}
+	}
+}
+
 func TestQueue(t *testing.T) {
 	var q *Queue
 
-	t.Run("NewQueue", func(t *testing.T) {
+	t.Run("new queue", func(t *testing.T) {
 		q = NewQueue(3)
 	})
 
 	e1, e2 := Element{Point: Point{1, 2}}, Element{Point: Point{3, 4}}
 
-	t.Run("Push", func(t *testing.T) {
+	t.Run("push", func(t *testing.T) {
 		q.Push(&e1)
 		q.Push(&e2)
+		expected := []*Element{&e1, &e2, nil}
+		checkQueue(t, q, expected)
 	})
 
-	t.Run("Pop", func(t *testing.T) {
-		popped := q.Pop()
-
-		if popped != &e1 {
-			t.Error("Pop returned wrong element")
-		}
+	t.Run("pop", func(t *testing.T) {
+		q.Pop()
+		expected := []*Element{nil, &e2, nil}
+		checkQueue(t, q, expected)
 	})
 
 	e3 := Element{Point: Point{5, 6}}
 
-	t.Run("Push again", func(t *testing.T) {
+	t.Run("push again", func(t *testing.T) {
 		q.Push(&e3)
+		expected := []*Element{nil, &e2, &e3}
+		checkQueue(t, q, expected)
 	})
 
 	e4 := Element{Point: Point{7, 8}}
 
-	t.Run("Push to the capacity with index wrap", func(t *testing.T) {
+	t.Run("push to the capacity with index wrap", func(t *testing.T) {
 		q.Push(&e4)
+		expected := []*Element{&e4, &e2, &e3}
+		checkQueue(t, q, expected)
 	})
 
-	t.Run("Remove", func(t *testing.T) {
+	t.Run("remove", func(t *testing.T) {
 		q.Remove(1)
-
 		expected := []*Element{nil, &e4, &e3}
-
-		for i := 0; i < q.capacity; i++ {
-			if q.elements[i] != expected[i] {
-				t.Error("Error")
-			}
-		}
+		checkQueue(t, q, expected)
 	})
 }
